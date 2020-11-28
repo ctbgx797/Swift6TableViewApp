f1bc61be845013eb1c7903c4108e29e5ca28b3a7
Yoshiki <villager2525@gmail.com>
2020-11-28 20:03:04 +0900

    first commit

---
+++ Swift6TableViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6TableViewApp
+//
+//  Created by 西谷恭紀 on 2020/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
