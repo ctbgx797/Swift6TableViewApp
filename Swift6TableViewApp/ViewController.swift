05d6536fa86c14b814ba9d62edb5d2ed22e143b3
Yoshiki <villager2525@gmail.com>
2020-11-28 20:20:55 +0900

    定義入れた

--- Swift6TableViewApp/ViewController.swift
+++ Swift6TableViewApp/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
